2a26c940255e65c933827c0b1533f4f7145d4ba2
hamzah <hamzah.alqata@gmail.com>
2024-04-23 23:38:06 +0300

    complate feedback screen

--- lib/features/client/feedback/presentation/pages/feedback_screen.dart
+++ lib/features/client/feedback/presentation/pages/feedback_screen.dart
@@ -3,6 +3,7 @@
 import 'dart:ffi';
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_locales/flutter_locales.dart';
 import 'package:image_cropper/image_cropper.dart';
@@ -23,6 +24,7 @@ class _FeedbackScreenState extends State<FeedbackScreen> {
   TextEditingController fullName = TextEditingController();
   TextEditingController phoneNumber = TextEditingController();
   TextEditingController emailAddress = TextEditingController();
+  TextEditingController whatsProblem = TextEditingController();
   String _selectedItem = 'Option 1';
 
   File? imageFile;
@@ -96,143 +98,214 @@ class _FeedbackScreenState extends State<FeedbackScreen> {
         },
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
 
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //space
-            SizedBox(height: 10,
-            width: double.infinity,),
-            //text
-            Text(Locales.string(context, "any_ques"),style: fontLargeBold,),
-            //text
-            Text(Locales.string(context, "any_ques_description"),style: fontMedium,),
-            //space
-            SizedBox(height: 40,),
-            //form
-            Form(
-                child: Column(
-                  children: [
-                    //name
-                    TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Theme.of(context).cardColor,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide.none,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //space
+              SizedBox(height: 10,
+              width: double.infinity,),
+              //text
+              Text(Locales.string(context, "any_ques"),style: fontLargeBold,),
+              //text
+              Text(Locales.string(context, "any_ques_description"),style: fontMedium,),
+              //space
+              SizedBox(height: 40,),
+              //form
+              Form(
+                  child: Column(
+                    children: [
+                      //name
+                      TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Theme.of(context).cardColor,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText:Locales.string(context, "full_name"),
                         ),
-                        hintText:Locales.string(context, "full_name"),
+                        controller:fullName,
+                        keyboardType: TextInputType.name,
                       ),
-                      controller:fullName,
-                      keyboardType: TextInputType.name,
-                    ),
-                    SizedBox(height: 15,),
-                    //phone
-                    TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Theme.of(context).cardColor,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide.none,
+                      SizedBox(height: 15,),
+                      //phone
+                      TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Theme.of(context).cardColor,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText:Locales.string(context, "phone"),
                         ),
-                        hintText:Locales.string(context, "phone"),
+                        controller:phoneNumber,
+                        keyboardType: TextInputType.phone,
                       ),
-                      controller:phoneNumber,
-                      keyboardType: TextInputType.phone,
-                    ),
-                    SizedBox(height: 15,),
-                    //email
-                    TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Theme.of(context).cardColor,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: BorderSide.none,
+                      SizedBox(height: 15,),
+                      //email
+                      TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Theme.of(context).cardColor,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide.none,
+                          ),
+                          hintText:Locales.string(context, "email"),
                         ),
-                        hintText:Locales.string(context, "email"),
+                        controller:emailAddress,
+                        keyboardType: TextInputType.emailAddress,
                       ),
-                      controller:emailAddress,
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    SizedBox(height: 15,),
-                    InkWell(
-                      onTap: _imgFromGallery,
-                      child: Container(
+                      SizedBox(height: 15,),
+                      InkWell(
+                        onTap: _imgFromGallery,
+                        child: Container(
+                          height: 65,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).cardColor,
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                          child:Align(
+                            alignment: (Locales.currentLocale(context).toString()=="ar")
+                                ?Alignment.centerRight
+                                :Alignment.centerLeft,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                  Locales.string(context, "add_image"),
+
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 15,),
+                      Container(
                         height: 65,
                         width: double.infinity,
                         decoration: BoxDecoration(
                             color: Theme.of(context).cardColor,
                             borderRadius: BorderRadius.circular(20)
                         ),
-                        child:Align(
+                        child: Align(
                           alignment: (Locales.currentLocale(context).toString()=="ar")
                               ?Alignment.centerRight
                               :Alignment.centerLeft,
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                                Locales.string(context, "add_image"),
+                            child: SizedBox(
+                              width: double.infinity,
+                              child: DropdownButton<String>(
+                                value: _selectedItem,
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    _selectedItem = newValue!;
+                                  });
+                                },
+                                icon: null,//
+                                underline: null, // Removes the underline border
+                                items: <String>['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 4', 'Option 4']
+                                    .map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value, style: fontMedium.copyWith(color: Colors.grey),),
 
+                                  );
+                                }).toList(),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 15,),
-                    Container(
-                      height: 65,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).cardColor,
-                          borderRadius: BorderRadius.circular(20)
-                      ),
-                      child: Align(
-                        alignment: (Locales.currentLocale(context).toString()=="ar")
-                            ?Alignment.centerRight
-                            :Alignment.centerLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SizedBox(
-                            width: double.infinity,
-                            child: DropdownButton<String>(
-                              value: _selectedItem,
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  _selectedItem = newValue!;
-                                });
-                              },
-                              icon: null,//
-                              underline: null, // Removes the underline border
-                              items: <String>['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 4', 'Option 4']
-                                  .map<DropdownMenuItem<String>>((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value, style: fontMedium.copyWith(color: Colors.grey),),
+                      SizedBox(height: 15,),
+                      ////////////////////////////////////////////////////////////////////
 
-                                );
-                              }).toList(),
-                            ),
+                      TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+
+                          fillColor: Theme.of(context).cardColor,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide.none,
                           ),
+                          hintText:Locales.string(context, "whats_problem"),
+
+                          contentPadding: EdgeInsets.symmetric(vertical: 35, horizontal: 8), // Adjust the vertical padding as needed
                         ),
+                        controller:whatsProblem,
+                        keyboardType: TextInputType.emailAddress,
                       ),
-                    )
-                    ////////////////////////////////////////////////////////////////////
+                      SizedBox(height: 25,),
+                    ],
+                  )
+              ),
+
+              //button
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                  height: 70,
+                  width: double.infinity,
+                  child: ElevatedButton(onPressed: (){},
+                      child: Text(Locales.string(context, "send"),style: fontLarge,)),
+                ),
+              )
 
 
 
 
 
-                  ],
-                )
-            ),
 
-            //button
-          ],
+
+
+
+
+
+
+
+
+
+
+
+
+              // InkWell(
+              //   onTap: _imgFromGallery,
+              //   child: Container(
+              //     height: 65,
+              //     width: 250,
+              //     decoration: BoxDecoration(
+              //         color: Theme.of(context).,
+              //         borderRadius: BorderRadius.circular(15)
+              //     ),
+              //     child:Align(
+              //       alignment: (Locales.currentLocale(context).toString()=="ar")
+              //           ?Alignment.centerRight
+              //           :Alignment.centerLeft,
+              //       child: Padding(
+              //         padding: const EdgeInsets.all(8.0),
+              //         child: Text(
+              //           Locales.string(context, "add_image"),
+              //
+              //         ),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+
+
+
+
+            ],
+          ),
         ),
       ),
 
