74cfa9eb24becf27e16e8e2e7fc6cbe05675580f
Mohsin-Alaini <mohsin.alaini2020@gmail.com>
2024-04-30 19:44:04 +0300

    empty review page

--- lib/features/client/favorite/presentation/pages/favorite_screen.dart
+++ lib/features/client/favorite/presentation/pages/favorite_screen.dart
@@ -23,69 +23,69 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(title: const Text("GridView with service")),
-      body: AddFavorite(),
-      // body: Column(
-      //   mainAxisAlignment: MainAxisAlignment.start,
-      //   crossAxisAlignment: CrossAxisAlignment.start,
-      //   mainAxisSize: MainAxisSize.min,
-      //   children: [
-      //     Flexible(
-      //       child: FutureBuilder<FavoriteModel>(
-      //         future: NetworkRequest.fetchPhotos(),
-      //         builder: (context, snapshot) {
-      //           if (snapshot.hasError) {
-      //             return Text('Error ${snapshot.error}');
-      //           } else if (snapshot.hasData) {
-      //             var data =  snapshot.data!.results!;
-      //             return GridView.builder(
-      //               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-      //                 crossAxisCount: 2,
-      //                 childAspectRatio: 1,
-      //                 mainAxisSpacing: 4,
-      //                 crossAxisSpacing: 4,
-      //               ),
-      //               itemCount: 5,
-      //               itemBuilder: (context, index) {
-      //                 return Padding(
-      //                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      //                   child: FavoriteProperty(
-      //                     //imageUrl: data[index].prop!.image!.first.image!,
-      //                   ),
-      //                 );
-      //
-      //               },
-      //             );
-      //
-      //           }
-      //           return GridView.builder(
-      //             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-      //               crossAxisCount: 2,
-      //               childAspectRatio: 1,
-      //               mainAxisSpacing: 4,
-      //               crossAxisSpacing: 4,
-      //             ),
-      //             itemCount: 5,
-      //             itemBuilder: (context, index) {
-      //               return Padding(
-      //                 padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      //                 child: FavoriteWidget(
-      //                   title: "hamzah",
-      //                   address: "sanna",
-      //                   imagePath: AppConstants.noImageUrl,
-      //                   price: "250000",
-      //                   isFavorite: true,
-      //                   rate: 3.2,
-      //                 ),
-      //               );
-      //
-      //             },
-      //           );
-      //         },
-      //       ),
-      //     ),
-      //   ],
-      // ),
-    );;
+      // body: AddFavorite(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Flexible(
+            child: FutureBuilder<FavoriteModel>(
+              future: NetworkRequest.fetchPhotos(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error ${snapshot.error}');
+                } else if (snapshot.hasData) {
+                  var data =  snapshot.data!.results!;
+                  return GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                    ),
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: FavoriteProperty(
+                          //imageUrl: data[index].prop!.image!.first.image!,
+                        ),
+                      );
+
+                    },
+                  );
+
+                }
+                return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1,
+                    mainAxisSpacing: 4,
+                    crossAxisSpacing: 4,
+                  ),
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: FavoriteWidget(
+                        title: "hamzah",
+                        address: "sanna",
+                        imagePath: AppConstants.noImageUrl,
+                        price: "250000",
+                        isFavorite: true,
+                        rate: 3.2,
+                      ),
+                    );
+
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
